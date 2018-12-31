a23e16561765fc8edd6d14d81a77de2f3e295f20
cowgill <david.cowgill@contractor.chanzuckerberg.com>
2018-12-31 14:53:11 -0800

    Add error pages & SQLAlchemy.

--- app/home/views.py
+++ app/home/views.py
@@ -9,7 +9,7 @@ def homepage():
     """
     Render the homepage template on the / route
     """
-    return render_template('home/index.html', title="Welcome")
+    return render_template('home/index.html')
 
 
 @home.route('/dashboard')
@@ -18,4 +18,4 @@ def dashboard():
     """
     Render the dashboard template on the /dashboard route
     """
-    return render_template('home/dashboard.html', title="Dashboard")
+    return render_template('home/dashboard.html')
