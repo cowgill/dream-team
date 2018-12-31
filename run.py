442b3a76dd10f0e6a34a7b199f6661f6fc23df07
cowgill <david.cowgill@contractor.chanzuckerberg.com>
2018-12-31 13:07:29 -0800

    App structure setup and working w/o db.

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
-from app import app
+import os
+
+from app import create_app
+
+# use the exported cli value OR set to dev by default
+config_name = os.getenv('FLASK_CONFIG', 'default')
+app = create_app(config_name)
+
 
 if __name__ == '__main__':
     app.run()
